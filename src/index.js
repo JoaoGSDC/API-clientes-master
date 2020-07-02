a233abcdda607b97b3be1632b9c2a1afa50f6c1c
Joao Cruz <joaogsdc@gmail.com>
2020-07-01 21:36:07 -0300

    Project #4

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const cors = require('cors');
 
 const app = express();
 
+const porta = process.env.PORT || 8080;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Server Online"));
+app.listen(porta, () => console.log("Server Online"));
